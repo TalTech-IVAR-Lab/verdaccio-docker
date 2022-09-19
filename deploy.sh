590b0a3270d3c96d29f80dd5c849642ac07d7e0f
Yevhen Bodnarenko <yevhen.bondarenko@taltech.ee>
2022-09-19 06:36:55 +0000

    Update deployment script
    - New directory names
    - Simplified function names

--- deploy.sh
+++ deploy.sh
@@ -15,11 +15,12 @@ BLINK='\e[33;5m'
 
 # Constants
 EMPTY_STRING=""
-VOLUMES_ROOT="verdaccio_volumes"
-CONFIG_VOLUME="${VOLUMES_ROOT}/conf/"
-STORAGE_VOLUME="${VOLUMES_ROOT}/storage/"
-PLUGINS_VOLUME="${VOLUMES_ROOT}/plugins/"
-LOGS_VOLUME="${VOLUMES_ROOT}/logs/"
+CADDY_ROOT="volumes/caddy"
+VERDACCIO_ROOT="volumes/verdaccio"
+CONFIG_VOLUME="${VERDACCIO_ROOT}/conf/"
+STORAGE_VOLUME="${VERDACCIO_ROOT}/storage/"
+PLUGINS_VOLUME="${VERDACCIO_ROOT}/plugins/"
+LOGS_VOLUME="${VERDACCIO_ROOT}/logs/"
 VERDACCIO_USER_UID=10001
 VERDACCIO_USER_GROUP=65533
 
@@ -34,11 +35,11 @@ VERDACCIO_EMAIL=""
 
 
 # Functions
-function_log_message() {
+log_message() {
   echo -e "${LIGHTGREEN}> $1${NOCOLOR}"
 }
 
-function_print_usage() {
+print_usage() {
   echo -e "
   Options:
       -h, --https      Add this flag to enable HTTPS.
@@ -49,18 +50,18 @@ function_print_usage() {
   "
 }
 
-function_ensure_package_installed() {
+ensure_package_installed() {
   PACKAGE_NAME=$1
   if dpkg --get-selections | grep -q "^${PACKAGE_NAME}[[:space:]]*install$" >/dev/null; then
-    function_log_message "${PACKAGE_NAME} is already installed."
+    log_message "${PACKAGE_NAME} is already installed."
   else
-    function_log_message "Installing ${PACKAGE_NAME}..."
+    log_message "Installing ${PACKAGE_NAME}..."
     sudo apt update
     sudo apt install -y ${PACKAGE_NAME}
   fi
 }
 
-function_create_directory_if_doesnt_exist() {
+create_directory_if_doesnt_exist() {
   if [ ! -d $1 ]; then
     mkdir -p $1;
   fi
@@ -96,44 +97,45 @@ done
 
 
 # Set environment variables in the .env file
-function_log_message "Setting environment variables:"
+log_message "Setting environment variables:"
 if $USE_HTTPS; then
   VERDACCIO_PROTOCOL=https
 fi
 sudo echo -e "VERDACCIO_PORT=${VERDACCIO_PORT}\nVERDACCIO_PROTOCOL=${VERDACCIO_PROTOCOL}\nVERDACCIO_DOMAIN=${VERDACCIO_DOMAIN}\nVERDACCIO_EMAIL=${VERDACCIO_EMAIL}" | sudo tee .env
 
 # Install Docker and Docker Compose
-function_ensure_package_installed "docker"
-function_ensure_package_installed "docker-compose"
+ensure_package_installed "docker"
+ensure_package_installed "docker-compose"
 
 # Create directories
-function_log_message "Creating directories..."
-function_create_directory_if_doesnt_exist ${CONFIG_VOLUME}
-function_create_directory_if_doesnt_exist ${STORAGE_VOLUME}
-function_create_directory_if_doesnt_exist ${PLUGINS_VOLUME}
-function_create_directory_if_doesnt_exist ${LOGS_VOLUME}
+log_message "Creating volume directories..."
+create_directory_if_doesnt_exist ${CONFIG_VOLUME}
+create_directory_if_doesnt_exist ${STORAGE_VOLUME}
+create_directory_if_doesnt_exist ${PLUGINS_VOLUME}
+create_directory_if_doesnt_exist ${LOGS_VOLUME}
+create_directory_if_doesnt_exist ${CADDY_ROOT}
 
 # Generate SSL certificates for HTTPS
 rm -rf https
-if $USE_HTTPS && $CREATE_HTTPS_CERTS; then
-  function_log_message "HTTPS is requested."
-  function_ensure_package_installed "openssl"
+if $USE_HTTPS && $CREATE_HTTPS_CERTS && false; then
+  log_message "HTTPS is requested."
+  ensure_package_installed "openssl"
 
-  function_log_message "Generating SSL certificates..."
+  log_message "Generating SSL certificates..."
   sudo openssl genrsa -out ${CONFIG_VOLUME}/verdaccio-key.pem 2048
   sudo openssl req -new -sha256 -key ${CONFIG_VOLUME}/verdaccio-key.pem -out ${CONFIG_VOLUME}/verdaccio-csr.pem
   sudo openssl x509 -req -in ${CONFIG_VOLUME}/verdaccio-csr.pem -signkey ${CONFIG_VOLUME}/verdaccio-key.pem -out ${CONFIG_VOLUME}/verdaccio-cert.pem
 fi
 
 # Copy our configuration into Verdaccio Docker volume
-function_log_message "Copying Verdaccio config to Docker volume..."
+log_message "Copying Verdaccio config to Docker volume..."
 sudo cp config.yaml ${CONFIG_VOLUME}/config.yaml
 
 # Generate password file
 sudo touch ${CONFIG_VOLUME}/dolcevita
 
 # Configure folder permissions to allow Verdaccio access
-sudo chown -R ${VERDACCIO_USER_ID}:${VERDACCIO_USER_ID} ${VOLUMES_ROOT}
+sudo chown -R ${VERDACCIO_USER_ID}:${VERDACCIO_USER_GROUP} ${VERDACCIO_ROOT}
 
 # Export environment vars required for docker-compose
 export VERDACCIO_PROTOCOL
@@ -143,5 +145,5 @@ export VERDACCIO_EMAIL
 export VERDACCIO_USER_UID
 
 # Re-deploy Verdaccio Docker to let it notice configuration changes
-function_log_message "Recreating Verdaccio docker-compose..."
+log_message "Recreating Verdaccio docker-compose..."
 docker-compose up -d --force-recreate
